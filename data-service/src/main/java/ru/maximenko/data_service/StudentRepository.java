d937e3959573d7fbde9410617cd393edba3179d1
aleksey-maksimenko <62542456+aleksey-maksimenko@users.noreply.github.com>
2025-11-20 14:58:55 +0300

    Шаг 3. Добавили код для работы с БД

---
+++ data-service/src/main/java/ru/maximenko/data_service/StudentRepository.java
@@ -0,0 +1,8 @@
+package ru.maximenko.data_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
+
